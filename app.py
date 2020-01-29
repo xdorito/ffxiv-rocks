ae3a419468bbf3c09ef0362476a95f6e4494f4c5
Robert Beaver <robertjbeaver>
2020-01-29 03:50:17 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from nameify import Nameify
+import re
+
+# configs
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+app.secret_key='rocks_ffxiv'
+db = SQLAlchemy(app)
+namer = Nameify()
+
+#db models
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long = db.Column(db.String(2044), nullable=False)
+    short = db.Column(db.String(100))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'My url is {self.long}'
+
+# routes
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        url_long = request.form['content']
+        found_url = Url.query.filter_by(long= url_long).first()
+        if found_url:
+            flash('Already known!')
+        else:
+            url_short = namer.generate_url()
+            url = re.sub("https?://", "", request.url_root)
+
+            new_url = Url(long = url_long, short = url_short)
+            try:
+                db.session.add(new_url)
+                db.session.commit()
+                flash(f'{url}{url_short}')
+            except:
+                return "Error adding url"
+            
+        return redirect('/')
+    else:
+        return render_template('index.html')
+
+@app.route('/<path:path>')
+def redirect_to_path(path):
+    # print(path)
+    found_url = Url.query.filter_by(short= path).first()
+    if found_url:
+        return redirect(f'http://{found_url.long}')
+    else:
+        flash(f'Url not found')
+        return redirect('/')
+
+# enable debug
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ nameify.py
@@ -0,0 +1,34 @@
+import csv
+import random
+import string
+
+class Nameify:
+
+    # lower case alpha numeric constant
+    ALNUM = string.printable[0:62]
+
+    def __init__(self, debug = False):
+        self.names = self.load_names()
+
+    def load_names(self, file = 'names.csv'):
+        names = []
+        with open(file) as names_file:
+            csv_reader = csv.reader(names_file)
+            for row in csv_reader:
+                names.append(row[0])
+        return names
+
+    def generate_url(self):
+        url = ''
+        name_pos = random.randint(1,5)
+        for i in range(1,6):
+            if i == name_pos:
+                if i == 1:
+                    url+= f'{random.choice(self.names)}-'
+                elif i == 5:
+                    url+= f'-{random.choice(self.names)}'
+                else:
+                    url+= f'-{random.choice(self.names)}-'
+            else:
+                url+= random.choice(self.ALNUM)
+        return url
